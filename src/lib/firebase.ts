5a2b0bc03ede985daedd22ad32fae2434f98c880
Akash Madhav . Y <akashmadhav@gmail.com>
2025-09-17 14:22:56 +0000

    Set up Firebase Backend

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,16 +3,8 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
+import { firebaseConfig } from "./firebase-config";
 
-// Hardcoded configuration to ensure correctness
-const firebaseConfig = {
-  apiKey: "AIzaSyD0wsmwziRfkE_PQLdx3s0oYMj2a9Ru8cY",
-  authDomain: "optifitaicopy-87674237-d47c0.firebaseapp.com",
-  projectId: "optifitaicopy-87674237-d47c0",
-  storageBucket: "optifitaicopy-87674237-d47c0.appspot.com",
-  messagingSenderId: "620460614828",
-  appId: "1:620460614828:web:5adecf67678f4467e7eb09",
-};
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
