9d8b29305eabbb6054aef6fead8814f06471577d
Akash Madhav . Y <akashmadhav@gmail.com>
2025-09-23 17:12:27 +0000

    remove this part from the home feature

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -83,20 +83,15 @@ interface Workout {
 function HomeDashboard({ userId, workouts, isLoadingHistory }: { userId: string, workouts: Workout[], isLoadingHistory: boolean }) {
   return (
     <div className="space-y-8">
-      <ProgressVisualization 
-        workouts={workouts} 
-        isLoading={isLoadingHistory} 
-      />
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+        <div className="lg:col-span-2">
+            <ProgressVisualization 
+                workouts={workouts} 
+                isLoading={isLoadingHistory} 
+            />
+        </div>
         <div className="lg:col-span-1">
           <HomeWorkoutLog userId={userId} onWorkoutLogged={() => {}}/>
-        </div>
-        <div className="lg:col-span-2">
-           <WorkoutAccomplishmentSummary 
-              userId={userId} 
-              workouts={workouts}
-              isLoading={isLoadingHistory}
-            />
         </div>
       </div>
     </div>
